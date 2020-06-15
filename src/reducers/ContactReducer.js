8b99056886ff1cd3e3b34c703b8f585cfc8dc730
Obinna Okoro <obinna.okoroeugene@gmail.com>
2020-06-15 09:59:15 +0100

    catch errors for contact us email

--- src/reducers/ContactReducer.js
+++ src/reducers/ContactReducer.js
@@ -6,16 +6,18 @@ export default function contactReducer(state = {}, action) {
             return Object.assign({}, state, {
                 isProcessing: true,
                 created: undefined,
+                error: undefined,
             })
         case constants.CONTACT_SUCCESS:
             return {
                 ...state,
                 isProcessing: false,
-                created: true
+                created: true,
+                error: undefined,
             };
         case constants.CONTACT_FAILURE:
             return Object.assign({}, state, {
-                error: `${action.payload.error}`,
+                error: `${(action.payload.response && action.payload.response.message) || (action.payload.message)}`,
                 created: undefined,
                 isProcessing: false
             })
